7ff498f6325d934b3a93cf73b8104e19729b4bf3
ARISH <mohdarish2001@gmail.com>
2024-02-01 22:13:10 +0530

    Initial commit

---
+++ Episode 01 - Inception/file/App.js
@@ -0,0 +1,14 @@
+const perent =React.createElement("div", 
+{id: "perent"},
+React.createElement("div", 
+{id:"child"},
+[React.createElement("h1", {} , "Hi my name is h1" ),
+React.createElement("h2", {} , "Hi my name is h2" )
+])
+);
+// const heading = React.createElement('h1', {} , "hello world with the help of react");
+
+console.log(perent); //object
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(perent);
